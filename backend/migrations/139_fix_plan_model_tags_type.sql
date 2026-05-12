d99b0f94a2d109efe93d6746c5a6e64d9ff8e7f7
ding.yu <dingyu@dingyudeMacBook-Pro.local>
2026-05-12 11:03:13 +0800

    fix: 修复迁移脚本，先删除默认值再修改类型

--- backend/migrations/139_fix_plan_model_tags_type.sql
+++ backend/migrations/139_fix_plan_model_tags_type.sql
@@ -1,7 +1,11 @@
 -- 修改 subscription_plans.model_tags 字段类型从 text[] 改为 jsonb
 -- 这样可以正确处理 JSON 序列化和反序列化
 
--- 先将现有的 text[] 数据转换为 jsonb
+-- 先删除默认值
+ALTER TABLE subscription_plans
+ALTER COLUMN model_tags DROP DEFAULT;
+
+-- 将现有的 text[] 数据转换为 jsonb
 ALTER TABLE subscription_plans
 ALTER COLUMN model_tags TYPE jsonb
 USING CASE
@@ -9,6 +13,6 @@ USING CASE
     ELSE to_jsonb(model_tags)
 END;
 
--- 设置默认值
+-- 设置新的默认值
 ALTER TABLE subscription_plans
 ALTER COLUMN model_tags SET DEFAULT '[]'::jsonb;
